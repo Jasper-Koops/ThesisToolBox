01edd3f8cfa4538b60dd9837474b2366d4e3abd9
Jasper Koops <jasperkoops91@gmail.com>
2017-11-05 14:04:10 +0100

    Extended the templates, Added a person createview with the relevant templates and forms

--- person_tracker/models.py
+++ person_tracker/models.py
@@ -7,10 +7,16 @@ class Nationality(models.Model):
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    middlename = models.CharField(max_length=255, blank=True)
+    lastname = models.CharField(max_length=255)
     added_on = models.DateField(auto_now=True)
     year_of_birth = models.DateField()
-    year_of_death = models.DateField(blank=True)
-    nationality = models.ForeignKey(Nationality, blank=True)
+    year_of_death = models.DateField(blank=True, null=True)
+    nationality = models.ForeignKey(Nationality, blank=True, null=True)
     branch = models.CharField(max_length=255)
     notes = models.ForeignKey(Note, blank=True, null=True)
+
+    @property
+    def full_name(self):
+        return self.firstname + " " + self.middlename + " " + self.lastname
--- person_tracker/views.py
+++ person_tracker/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from person_tracker.models import Person, Nationality
 # Create your views here.
 
-class Home(TemplateView):
-    template_name = "person_tracker/person_tracker_home.html"
+class Home(ListView):
+    #template_name = "person_tracker/person_tracker_home.html"
+    model = Person
+    template_name = "person_tracker/person_overview.html"
+
+
+class PersonOverview(ListView):
+    model = Person
+    template_name = "person_tracker/person_overview.html"
+
+
+class PersonCreate(CreateView):
+    model = Person
+    success_url = reverse_lazy('person_overview')
+    fields = ['firstname', 'middlename', 'lastname', 'year_of_birth', 'year_of_death', 'nationality', 'branch']
+    template_name = "person_tracker/person_create.html"
+
+
+class NationalityOverview(ListView):
+    model = Person
+    template_name = "person_tracker/nationality_overview.html"
