ca190fbbf2f4314e433ab6e22e44b231d9686dc7
Jasper Koops <jasperkoops91@gmail.com>
2017-11-06 17:02:20 +0100

    Switched database to postgresql

--- ThesisToolBox/settings.py
+++ ThesisToolBox/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ThesisToolBox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thesistoolbox',
+        'USER': 'jasper',
+        'PASSWORD': 'T440sLetmein3003',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
