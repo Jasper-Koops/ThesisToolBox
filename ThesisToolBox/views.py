1628860b726355e518223da3f344efca573e1bdf
Jasper Koops <jasperkoops91@gmail.com>
2017-11-13 18:40:22 +0100

    Added basic bootstrap (4!) integration, started working on the index overview page.

--- ThesisToolBox/views.py
+++ ThesisToolBox/views.py
@@ -1,4 +1,12 @@
 from django.views.generic import TemplateView
+from source_tracker.models import Book, Article, Pamphlet
+from person_tracker.models import Person
+from notes.models import Note, Tag
 
 class Index(TemplateView):
     template_name = 'ThesisToolBox/index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super(Index, self).get_context_data(**kwargs)
+        data['notes'] = Note.objects.all().order_by('-id')[:5]
+        return data
