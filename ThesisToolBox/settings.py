3986f40860455d3c18e108ce37b794d4a38f2274
Jasper Koops <jasperkoops91@gmail.com>
2017-12-04 18:07:23 +0100

    Added requirements file, minor template edit.

--- ThesisToolBox/settings.py
+++ ThesisToolBox/settings.py
@@ -137,6 +137,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'assets'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'root_static/')
 
 WEBPACK_LOADER = {
     'DEFAULT': {
