57d642e140cca7d44151d7dd134c675975f36c92
Jasper Koops <jasperkoops91@gmail.com>
2017-12-10 16:23:23 +0100

    Removed the old JS/React files from the project

--- assets/js/pages/create.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BookCreate from '../components/book_create';
-
-
-ReactDOM.render(<BookCreate />, document.getElementById('root'));
-
--- assets/js/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Welcome from '../components/home';
-
-
-ReactDOM.render(<Welcome />, document.getElementById('root'));
-
