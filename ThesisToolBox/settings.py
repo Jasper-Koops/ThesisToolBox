615780f8930c70613cca49c627cb2749ea1254e9
Jasper Koops <jasperkoops91@gmail.com>
2017-12-17 16:09:12 +0100

    Fixed the 'static_file' settings.

--- ThesisToolBox/settings.py
+++ ThesisToolBox/settings.py
@@ -135,19 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static_root/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
 
 DEBUG_TOOLBAR_PANELS = [
     'debug_toolbar.panels.versions.VersionsPanel',
